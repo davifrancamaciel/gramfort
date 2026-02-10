63645928416220142ebf9e07830a0f36c188096c
Davi Maciel <davi.maciel@nagra.com>
2026-02-10 16:45:35 -0300

    Otimização de upload de imagens

--- frontend/src/components/Logo/styles.ts
+++ frontend/src/components/Logo/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   img {
     height: 32px;
+    max-width: 180px;
     margin-right: 5px;
   }  
 `;
--- frontend/src/components/UploadImages/index.tsx
+++ frontend/src/components/UploadImages/index.tsx
@@ -7,50 +7,71 @@ function getBase64(file: any) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
+    // reader.onload = () => resolve(reader.result);
+    reader.onload = () => resize(file, resolve);
     reader.onerror = (error) => reject(error);
   });
 }
 
-function resize(file: any) {
-  return new Promise((resolve, reject) => {
-    var MAX_WIDTH = 800;
-    var MAX_HEIGHT = 600;
+var MAX_WIDTH = 800;
+var MAX_HEIGHT = 600;
 
-    const img = new Image();
-    img.src = file;
+function resize(image: any, callback: any) {
+  //criamos img que será a nossa imagem nova
+  const img = new Image();
 
-    let width = img.width;
-    let height = img.height;
+  //criamos um reader que lerá a nossa imagem
+  const reader = new FileReader();
 
-    if (width > height) {
-      if (width > MAX_WIDTH) {
-        height *= MAX_WIDTH / width;
-        width = MAX_WIDTH;
-      }
-    } else {
-      if (height > MAX_HEIGHT) {
-        width *= MAX_HEIGHT / height;
-        height = MAX_HEIGHT;
-      }
-    }
 
+  //vamos escrever o método do onload do reader
+  //porque ele é chamado no momento em que é finalizado a carregamento da imagem
+  reader.onload = (e: any) => {
+    img.src = e.target.result;
+
+    //quando a imagem fora carregada após receber o a linha superior
     img.onload = () => {
-      // cria canvas com dimensões desejadas
+      //criamos um canvas
       const canvas = document.createElement('canvas');
+      let ctx = canvas.getContext('2d');
+      //desenhamos a imagem na posição (0,0)
+      ctx?.drawImage(img, 0, 0);
+
+      //fazemos calculos para saber  qual lado é maior e reduzir na propoção certa
+      let width = img.width;
+      let height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      //definimos tamanho do canvas no mesmo tamanho que a imagem já reduzida
       canvas.width = width;
       canvas.height = height;
+      //pegamos o contexto do canvas
+      ctx = canvas.getContext('2d');
 
-      const ctx = canvas.getContext('2d');
-      // desenha a imagem redimensionada
+      //desenhamos a nova imagem passando a imagem, a posição de inicio e o tamanho
       ctx?.drawImage(img, 0, 0, width, height);
 
-      // retorna como base64 (pode usar toBlob se preferir)
-      resolve(canvas.toDataURL('image/jpeg', 0.9));
-    };
+      //aqui é onde a magia acontece,
 
-    img.onerror = (err) => reject(err);
-  });
+      // primeiro convertemos a imagem desenhada pelo canvas para o formato Blob
+      // o primeiro parametro é a função de callback que ele irá chamar após converte,
+      // o segundo o type da imagem
+      // e o terceiro é a qualidade variando de 0 a 1   
+      callback(ctx?.canvas.toDataURL(image.type, 0.8));
+    };
+  };
+
+  reader.readAsDataURL(image);
 }
 
 const UploadImages: React.FC<PropTypes> = (props) => {
@@ -69,8 +90,6 @@ const UploadImages: React.FC<PropTypes> = (props) => {
       for (let i = 0; i < file.fileList.length; i++) {
         const element = file.fileList[i];
         const preview = await getBase64(element.originFileObj);
-        // const d = await resize(preview);
-        // console.log(d)
         files.push({ ...element, preview, thumbUrl: preview });
       }
       props.setFileList(files);
